d8659c1fbf9976c76e8c552cd434ee49b278cd3c
PamiFalade <jesupamilerin.falade@ucalgary.ca>
2024-09-07 12:44:48 +0100

    Installed more dependencies
    Installed express, cors, morgan, body-parser, mysql2 (the MySQL driver), sequelize and sequelize-cli

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+const app = express();
+app.use(morgan('combined'));    // Log generator
+app.use(bodyParser.json());     // Allows the express app to easily parse any JSON requests that are sent in
+app.use(cors());                // Allows any host or client to access this app
+
 console.log("Hello World!!");
